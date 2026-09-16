727135087a65f3cebbee1eabd9f61a23de8edaf1
Ark223 <31862947+Ark223@users.noreply.github.com>
2026-09-16 12:33:22 +0200

    v2026.09.16 - Fixed conflict with BepHax

--- src/main/java/hyperglide/Hyperglide.java
+++ src/main/java/hyperglide/Hyperglide.java
@@ -2,72 +2,115 @@ package hyperglide;
 
 import com.mojang.logging.LogUtils;
 import hyperglide.hud.TruePing;
-import hyperglide.modules.AirPlace;
-import hyperglide.modules.AutoPilot;
-import hyperglide.modules.AutoWeb;
-import hyperglide.modules.BlockFarm;
-import hyperglide.modules.BounceFly;
-import hyperglide.modules.ControlFly;
-import hyperglide.modules.DeepTrace;
-import hyperglide.modules.EasyAccess;
-import hyperglide.modules.ElytraTweaks;
-import hyperglide.modules.FastFrame;
-import hyperglide.modules.FastPortal;
-import hyperglide.modules.FD3Crafter;
-import hyperglide.modules.MiningTweaks;
-import hyperglide.modules.Navigation;
-import hyperglide.modules.NoSprintFov;
-import hyperglide.modules.Overview;
-import hyperglide.modules.RaidStarter;
-import hyperglide.modules.RocketBoost;
-import hyperglide.modules.Scaffolding;
-import hyperglide.modules.SelfTrapper;
-import hyperglide.modules.TriggerBot;
+import hyperglide.modules.*;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
+import java.util.function.Supplier;
 
 public class Hyperglide extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("Hyperglide");
     public static final HudGroup HUD_GROUP = new HudGroup("Hyperglide");
 
+    private static final Entry[] MODULES = {
+        new Entry(AirPlace::new),
+        new Entry(AutoPilot::new),
+        new Entry(AutoWeb::new),
+        new Entry(BlockFarm::new),
+        new Entry(BounceFly::new),
+        new Entry(ControlFly::new, "bephax"),
+        new Entry(DeepTrace::new),
+        new Entry(EasyAccess::new),
+        new Entry(ElytraTweaks::new),
+        new Entry(FastFrame::new),
+        new Entry(FastPortal::new),
+        new Entry(FD3Crafter::new),
+        new Entry(ForceLog::new),
+        new Entry(MiningTweaks::new),
+        new Entry(Navigation::new),
+        new Entry(NoCoordLeak::new),
+        new Entry(NoSprintFov::new),
+        new Entry(Overview::new),
+        new Entry(RaidStarter::new),
+        new Entry(RocketBoost::new),
+        new Entry(Scaffolding::new),
+        new Entry(SelfTrapper::new),
+        new Entry(TriggerBot::new)
+    };
+
+    /**
+     * Initializes modules and HUD elements.
+     */
     @Override
     public void onInitialize() {
         LOG.info("Initializing Hyperglide");
         Hud.get().register(TruePing.info);
 
-        Modules.get().add(new AirPlace());
-        Modules.get().add(new AutoPilot());
-        Modules.get().add(new AutoWeb());
-        Modules.get().add(new BlockFarm());
-        Modules.get().add(new BounceFly());
-        Modules.get().add(new ControlFly());
-        Modules.get().add(new DeepTrace());
-        Modules.get().add(new EasyAccess());
-        Modules.get().add(new ElytraTweaks());
-        Modules.get().add(new FastFrame());
-        Modules.get().add(new FastPortal());
-        Modules.get().add(new FD3Crafter());
-        Modules.get().add(new MiningTweaks());
-        Modules.get().add(new Navigation());
-        Modules.get().add(new NoSprintFov());
-        Modules.get().add(new Overview());
-        Modules.get().add(new RaidStarter());
-        Modules.get().add(new RocketBoost());
-        Modules.get().add(new Scaffolding());
-        Modules.get().add(new SelfTrapper());
-        Modules.get().add(new TriggerBot());
+        for (Entry entry : MODULES) {
+            this.add(entry);
+        }
     }
 
+    /**
+     * Registers the main module category.
+     */
     @Override
     public void onRegisterCategories() {
         Modules.registerCategory(CATEGORY);
     }
 
+    /**
+     * Registers a module when no conflicting addon is loaded.
+     *
+     * @param entry module entry
+     */
+    private void add(Entry entry) {
+        if (this.blocked(entry)) return;
+        Modules.get().add(entry.make().get());
+    }
+
+    /**
+     * Checks whether an entry conflicts with a loaded addon.
+     *
+     * @param entry module entry
+     * @return true when a conflicting addon is loaded
+     */
+    private boolean blocked(Entry entry) {
+        for (String id : entry.mods()) {
+            if (this.loaded(id)) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Checks whether a specific Fabric mod is loaded.
+     *
+     * @param id mod identifier
+     * @return true when the mod is loaded
+     */
+    private boolean loaded(String id) {
+        return FabricLoader.getInstance().isModLoaded(id);
+    }
+
+    /**
+     * Stores a module factory and conflicting addons.
+     *
+     * @param make module factory
+     * @param mods conflicting addon identifiers
+     */
+    private record Entry(Supplier<Module> make, String... mods) {}
+
+    /**
+     * Returns the root package used by addon.
+     *
+     * @return addon package name
+     */
     @Override
     public String getPackage() {
         return "hyperglide";
